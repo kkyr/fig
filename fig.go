650a93045ae6a6ac1ad1d9bf57eecd848c3e73c4
Codey Oxley <codey.oxley@bluecore.com>
2025-06-03 17:17:51 -0400

    field.path: make squash-aware (#39)
    * field.path: make squash-aware
    Since fig relies on mapstructure, fields can be annotated
    with `",squash"` to flatten the config. It's super helpful
    for base settings that are embedded.
    
    However, because the environment variable resolution relies
    on field.path(), it doesn't match the config-file structure.
    
    This makes field.path() omit squashed fields.
    
    Example config that's affected:
    
    ```yml
    env: prod
    logging:
      level: info
    ```
    
    Example structs:
    
    ```go
    type Base struct {
      Env string `fig:"env"`
      Logging struct {
        Level string `fig:"level"`
      } `fig:"logging"`
    }
    
    type Config struct {
      Base `fig:",squash"`
    }
    ```
    
    Overriding with env var before and after this change:
    
    ```bash
    BASE_ENV=staging # before
    ENV=staging      # after
    ```
    
    * docs: include squash section

--- fig.go
+++ fig.go
@@ -227,7 +227,8 @@ func stringToRegexpHookFunc() mapstructure.DecodeHookFunc {
 	return func(
 		f reflect.Type,
 		t reflect.Type,
-		data interface{}) (interface{}, error) {
+		data interface{},
+	) (interface{}, error) {
 		if f.Kind() != reflect.String {
 			return data, nil
 		}
@@ -278,7 +279,7 @@ func (f *fig) processCfg(cfg interface{}) error {
 
 	for _, field := range fields {
 		if err := f.processField(field); err != nil {
-			errs[field.path()] = err
+			errs[field.path(f.tag)] = err
 		}
 	}
 
@@ -297,7 +298,7 @@ func (f *fig) processField(field *field) error {
 	}
 
 	if f.useEnv {
-		if err := f.setFromEnv(field.v, field.path()); err != nil {
+		if err := f.setFromEnv(field.v, field.path(f.tag)); err != nil {
 			return fmt.Errorf("unable to set from env: %w", err)
 		}
 	}
